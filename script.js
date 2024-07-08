8dae3c9f64ab679061569f560790924531aa3df5
Luka <luka.luka.luka93@gmail.com>
2024-07-08 14:12:07 -0700

    final word

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("ni")
+const myString = "developer";
+
+const bigD = (myString.substring(0, 1)).toUpperCase();
+const withoutD = myString.slice(1);
+const finalWord = bigD + withoutD
+console.log(finalWord)

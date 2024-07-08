a85fe6f983ba6b892e0b886e43c1b8d3c92daef5
Luka <luka.luka.luka93@gmail.com>
2024-07-08 14:18:31 -0700

    final word where D of developer is uppercase, using toUpperCase, substring and concat

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const myString = "developer";
 
-const bigD = (myString.substring(0, 1)).toUpperCase();
-const withoutD = myString.slice(1);
-const finalWord = bigD + withoutD
+// const bigD = (myString.substring(0, 1)).toUpperCase();
+// const withoutD = myString.slice(1);
+// const finalWord = bigD + withoutD
+// console.log(finalWord)
+
+const bigD = (myString[0]).toUpperCase();
+const word = myString.substring(1);
+const finalWord = bigD.concat(word)
 console.log(finalWord)

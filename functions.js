76f4ed431c22ecaac4d0b159e12525cf9042923a
Luka <luka.luka.luka93@gmail.com>
2024-07-10 13:59:19 -0700

    block global scope

--- functions.js
+++ functions.js
@@ -19,7 +19,7 @@ const result = subtract(10, 2);
 
 
 
-     // Parameters and Arguments ************ 
+     // *****        Parameters and Arguments       ****** 
 
 // Parameters are the variables that are defined in the function
 // Default Params
@@ -28,7 +28,7 @@ function registerUser(user = 'Bot') {
 }
 // console.log(registerUser());
 
-// Rest Parameters
+// *****        Rest Parameters        *****
 // Rest Parameters accept any number of arguments
 function sum(...numbers) { //I can use unlimited number of arguments and it will be put in an array
     let total = 0;
@@ -39,7 +39,8 @@ function sum(...numbers) { //I can use unlimited number of arguments and it will
 }
 // console.log(sum(1, 2, 3));   // 6
 
-// Objects as params
+
+// *****        Objects as params        *****
 function loginUser(user) {
     return `The user ${user.name} with the id of ${user.id} is logged in`;
 }
@@ -53,12 +54,12 @@ const user = {
 //     name: 'Sara'
 // })) // The user Sara with the id of 2 is logged in
 
-// Arrays as params
+// *****        Arrays as params
 function getRandom(arr) {
     const randomIndex = Math.floor(Math.random() * arr.length); // getting a random index
 
     const item = arr[randomIndex];
-    console.log(item)
+    // console.log(item)
 }
 getRandom([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) // it will give a random number between 1-10
 // if the (arr) parameter is like that, but if we use (...arr) then its:
@@ -67,23 +68,47 @@ getRandom([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) // it will give a random number betwe
 // it turns it automatically into an array
 
 
-// Global & Function Scope
+// *****        Global & Function Scope        *****
 
 const x = 100;
-console.log(x, 'in global')
+// console.log(x, 'in global')
 function run() {
-    console.log(window.innerHeight);
-    console.log(x, 'in function')
+    // console.log(window.innerHeight);
+    // console.log(x, 'in function')
 }
 run()
 if (true) {
-    console.log(x, 'in block')
+    // console.log(x, 'in block')
 };
 
 function add() {
     const y = 50; // function scoped, wont work outside of this function
     const x = 1;
-    console.log(x + y) //x in this function overshadows the x thats outside this function 
-
+    // console.log(x + y) //x in this function overshadows the x thats outside this function 
 }
 add()
+
+// *****        BLOCK GLOBAL SCOPE        *****
+const q = 100;
+
+if(true) {
+    const y = 200;
+    // console.log(q + y) // 300
+}
+
+for(let i = 0; i <= 10; i++) {
+    console.log(i) // 0 and then 1 then 2, 3, 4, 5, 6, 7, 8, 9, 10
+}
+// console.log(i) // error, i is not defined, because its only inside the block, not global
+if(true) {
+    const a = 500;
+    let b = 600;
+    var c = 700;
+}
+// console.log(a); error 
+// console.log(b); error 
+// console.log(c) // 700 since var is global scope, u want ur variables to be block scope const and let
+// CONST and Let are block scoped, which means i can acces them outside if, loops, but not outside functions
+
+
+// *****        Nested Scope        *****

cb6b5d564ff225ae4fe5e8178e3465dd01b1ad4f
Luka <luka.luka.luka93@gmail.com>
2024-07-11 16:58:36 -0700

    else if & nesting, and and or

--- execution-Context.js
+++ execution-Context.js
@@ -2,6 +2,7 @@
 // The Call Stack
 // Logic & Control Flow
 // Else-IF & Nesting
+// SWITCH STATEMENTS
 
 /* Execution Context is when you run any JavaScript, a special environment is created to handle
 the transformation & execution of code. This is called the Execution context.
@@ -177,3 +178,43 @@ if (q !== w) {
 
 
 // Else If & Nesting
+
+const d = new Date(10, 30, 2022, 6, 0, 0)  // Saturday January 12 1918 08:00
+const hour = d.getHours(); // 8
+
+// if (hour < 12) {
+//     console.log('Good Morning')
+// } else if (hour < 18) {
+//     console.log('Good Afternoon')
+// } else {
+//     console.log('Good Night')
+// }
+
+// Nested IF
+if (hour < 12) {
+    console.log('Good Morning');
+
+    if(hour === 6) {
+        console.log('Wake up')
+    }
+} else if (hour < 18) {
+    console.log('Good Afternoon')
+} else {
+    console.log('Good Night');
+
+    if(hour >= 20) {
+        console.log('zzzzzz')
+    }
+}
+
+// Multiple conditions in IF statement
+if (hour >= 7 && hour < 15) {     // && - and
+    console.log('It is work time!')
+}
+
+if (hour === 6 || hour === 20) {  // || - or
+    console.log('Brush your teeth')
+}
+
+
+//  SWITCH STATEMENTS

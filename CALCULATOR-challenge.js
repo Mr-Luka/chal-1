80ffa2f1644509d7894e436f4fb28842aa8ba516
Luka <luka.luka.luka93@gmail.com>
2024-07-15 14:05:01 -0700

    second solution to the calculator

--- CALCULATOR-challenge.js
+++ CALCULATOR-challenge.js
@@ -18,10 +18,27 @@ function calculator(num1, num2, operator) {
     }
     return result;
 }
-
-
 console.log(calculator(2, 2, '+'));
 console.log(calculator(8, 2, '-'));
 console.log(calculator(5, 5, '*'));
 console.log(calculator(10, 2, '/'));
 console.log(calculator(1, 2, 'x'));
+
+// Other way to do it
+const cal = (num1, num2, operator) => {
+    let result;
+    if (operator === '+'){
+        result = num1 + num2;
+    } else if (operator === "-"){
+        result = num1 - num2;
+    } else if (operator === "*"){
+        result = num1 * num2;
+    } else if (operator === "/") {
+        result = num1 / num2;
+    } else {
+        result = 'Invalid Operator'
+    }
+    return result;
+}
+console.log(cal(3, 5, '+'))
+console.log(cal(12,4, '/'))

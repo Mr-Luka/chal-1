db4ed71f300aa1012867a03d6cbd4b4d134471d0
Luka <luka.luka.luka93@gmail.com>
2024-07-16 13:52:35 -0700

    do while loop

--- Loops/while&do-While-loop.js
+++ Loops/while&do-While-loop.js
@@ -1,6 +1,7 @@
 // While Loop
 // Loop over arrays - While Loop
 // Nesting While Loops
+// DO... while Loops
 
 // Old way of doing loops, since now we have high order array method, .forEach(), .forIn() and .forOf().
 // important to know this becasue its fundemental
@@ -25,13 +26,54 @@ let i = 1;
 
 
 // Nesting While Loops
-while (i <= 5) {
-    console.log('Number ' + i);
 
-    let j = 1;
-    while (j <= 5) {
-        console.log(`${i} * ${j} = ${i * j}`);
-        j++;
-    }
+// while (i <= 5) {
+//     console.log('Number ' + i);
+
+//     let j = 1;
+//     while (j <= 5) {
+//         console.log(`${i} * ${j} = ${i * j}`);
+//         j++;
+//     }
+//     i++;
+// }
+/*
+Number 1
+ 1 * 1 = 1
+ 1 * 2 = 2
+ 1 * 3 = 3
+ 1 * 4 = 4
+ 1 * 5 = 5
+ Number 2
+ 2 * 1 = 2
+ 2 * 2 = 4
+ 2 * 3 = 6
+ 2 * 4 = 8
+ 2 * 5 = 10
+...... until and including Number 5
+*/
+
+
+
+//  ****         DO... while Loops           ****
+
+// do while loop will run at least once even if the condition is false
+// The question: when do I want to use a do while loop? is when you always want the block
+// of code to run at least once even if the condition is never met.
+
+do {
+    console.log('Number ' + i);
     i++;
-}
+} while (i <= 20);     // this is where we want to put our condition
+// This code is always gonna run once, even if the condition (while) is not true
+// Number 1
+// Number 2 ...
+// ...Number 20
+
+// Example with a condition that is not true
+let j = 21;
+do {
+    console.log("Number " + j);
+} while (j <= 20);
+// This condition is never met, but it will run once no matter what and show:
+// Number 21

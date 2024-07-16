0cc1c8d57dd89aee07b4dc1249b7e995e6e44e69
Luka <luka.luka.luka93@gmail.com>
2024-07-16 14:18:59 -0700

    same fizzBuzz challenge with while loop

--- Loops/fizzBuzz-Challenge.js
+++ Loops/fizzBuzz-Challenge.js
@@ -6,18 +6,36 @@
 
 The way that we can figure out if it's a multiple of three is if the the
 remainder is 0, and we can find the remainder using the modulus operator.
+we have to check first for FizzBuzz because it overlaps the Fizz and Buzz, so when
+whe check that first, we can check for Fizz and then Buzz
 */
 
 
 // For Loop
-for (let i = 1; i <= 100; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//         console.log('FizzBuzz');
+//     } else if( i % 3 === 0) {
+//         console.log('Fizz');
+//     } else if (i % 5 === 0) {
+//         console.log('Buzz');
+//     } else {
+//         console.log(i)
+//     }
+// }
+
+
+// While Loop
+let j = 1;
+while (j <= 100) {
+    if (j % 3 === 0 && j % 5 === 0){
         console.log('FizzBuzz');
-    } else if( i % 3 === 0) {
-        console.log('Fizz');
-    } else if (i % 5 === 0) {
-        console.log('Buzz');
+    } else if (j % 3 === 0) {
+        console.log('Fizz')
+    } else if (j % 5 === 0) {
+        console.log('Buzz')
     } else {
-        console.log(i)
+        console.log(j);
     }
+    j++;
 }

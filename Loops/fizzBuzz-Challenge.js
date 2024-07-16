a3a1c303cc85875432bac3b81210bf0285a04122
Luka <luka.luka.luka93@gmail.com>
2024-07-16 13:55:17 -0700

    setting up fizzBuzz challenge

---
+++ Loops/fizzBuzz-Challenge.js
@@ -0,0 +1,10 @@
+/*
+
+- Pring/ log the numbers from 1 to 100
+- For multiple of three print 'Fizz' instead of the number
+- For multiple of five print 'Buzz'
+- For numbers which are multiples of both three and five print 'FizzBuzz'
+
+
+
+*/

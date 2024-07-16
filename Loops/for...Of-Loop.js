d1f047fdd1700ff8b9a898f29df6b904cf465e74
Luka <luka.luka.luka93@gmail.com>
2024-07-16 14:30:43 -0700

    for of loop with arrays and with arrays of objects

---
+++ Loops/for...Of-Loop.js
@@ -0,0 +1,43 @@
+// For... Of Loop
+
+/*
+For...Of Loop is newer way to basically Loop through iterable objects like Arrays
+or even Strings and Maps and Sets.
+
+Cleaner way to loop throught an array thena standard for...Loop or while...Loop
+*/
+
+const items = ['book', 'table', 'chair', 'kite'];
+/* for loop, older way
+for (let i = 0; i < items.length; i++) {
+    console.log(items[i])
+}
+*/
+
+for (let item of items) {
+    console.log(item)
+}
+/* Get the same result and its much cleaner and more modern looking:
+book
+table
+chair
+kite
+*/
+
+// array with objects
+const users = [
+    {name: 'Brad'},
+    {name: 'Kate'},
+    {name: 'Steve'},
+];
+
+for (let user of users) {
+    console.log(user) 
+    // {name: 'Brad'}
+    // {name: 'Kate'}
+    // {name: 'Steve'}
+    console.log(user.name);
+    // Brad
+    // Kate
+    // Steve
+}

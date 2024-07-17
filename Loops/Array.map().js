b3272938a99f0e931bf68b582b7103d502fb1a27
Luka <luka.luka.luka93@gmail.com>
2024-07-17 13:24:53 -0700

    map through company names

--- Loops/Array.map().js
+++ Loops/Array.map().js
@@ -15,3 +15,29 @@ const doubledNumbers2 = numbers2.map(number=> `Number ` + number)
 //  If i were to use {} i would need to use Return keyword
 // has to be numbers2, or it will be an error i think
 console.log(doubledNumbers2) //  ['Number 1', 'Number 2', 'Number 3', 'Number 4', 'Number 5']
+
+// Same with .forEach()
+const doubleNumbers3 = [];
+numbers2.forEach(number=> {
+    doubleNumbers3.push(number * 2);
+})
+console.log(doubleNumbers3); // [2, 4, 6, 8, 10]
+
+
+
+// Example 3:
+const companies2 = [
+    {name: 'Company One', category: 'Finance', start: 1981, end: 2004},
+    {name: 'Company Two', category: 'Retail', start: 1992, end: 2008},
+    {name: 'Company Three', category: 'Auto', start: 1999, end: 2007},
+    {name: 'Company Four', category: 'Retail', start: 1989, end: 2010},
+    {name: 'Company Five', category: 'Technology', start: 2009, end: 2014},
+    {name: 'Company Six', category: 'Finance', start: 1987, end: 2010},
+    {name: 'Company Seven', category: 'Auto', start: 1986, end: 1996},
+    {name: 'Company Eight', category: 'Technology', start: 2011, end: 2016},
+    {name: 'Company Nine', category: 'Retail', start: 1981, end: 1989},
+];
+
+// Create an array of company names
+const companyNames2 = companies2.map(company=> company.name);
+console.log(companyNames2);

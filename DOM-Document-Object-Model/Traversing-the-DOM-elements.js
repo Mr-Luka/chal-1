38b84686d856eb4d1b2c5dcac82937990f718f4b
Luka <luka.luka.luka93@gmail.com>
2024-07-22 16:45:54 -0700

    traversing the dom

---
+++ DOM-Document-Object-Model/Traversing-the-DOM-elements.js
@@ -0,0 +1,12 @@
+ // Traversing - travel across or through
+ /*
+Relationships between element Nodes.
+Everything in the DOM is a node and there are many types of nodes: text node, comments nodes...
+There's certain properties if you want to work with those types of nodes
+ */
+
+/* ELEMENT NODES
+Any HTML tag on the page is an Element 
+
+
+*/

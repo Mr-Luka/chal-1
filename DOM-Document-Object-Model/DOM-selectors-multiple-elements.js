55b7d271c4e7f0b10bf78faa5d4d5346422608a1
Luka <luka.luka.luka93@gmail.com>
2024-07-22 14:29:38 -0700

    .forEach only on node list

--- DOM-Document-Object-Model/DOM-selectors-multiple-elements.js
+++ DOM-Document-Object-Model/DOM-selectors-multiple-elements.js
@@ -13,20 +13,32 @@ console.log(listItems[1].innerText) // Orange Juice
 // on a Node List, style is for a specific element, so If I wanted to change all of them
 // I could use .forEach() loop
 
-listItems.forEach((item, index)=> {
-    item.style.color = 'red'; // colors all item lists into red
+// listItems.forEach((item, index)=> {
+//     item.style.color = 'red'; // colors all item lists into red
     
-    if (index === 1){
-        item.remove() // it removes one item
-    } 
-    if(index === 0) {
-        // item.innerText = 'Oranges' // It changed the text Into Oranges, however:
-        // it also removed the X sign for delete, becasue that item list also includes
-        // the Delete button with that X logo inside, so what we can do is:
-        item.innerHTML = `Oranges
-          <button class="remove-item btn-link text-red">
-            <i class="fa-solid fa-xmark"></i>
-          </button>`;
+//     if (index === 1){
+//         item.remove() // it removes one item
+//     } 
+//     if(index === 0) {
+//         // item.innerText = 'Oranges' // It changed the text Into Oranges, however:
+//         // it also removed the X sign for delete, becasue that item list also includes
+//         // the Delete button with that X logo inside, so what we can do is:
+//         item.innerHTML = `Oranges
+//           <button class="remove-item btn-link text-red">
+//             <i class="fa-solid fa-xmark"></i>
+//           </button>`;
+//     }
+// }) 
 
-    }
-}) 
+const listItems2 = document.getElementsByClassName('item'); // HTMLCollections(4) [li.item, ...]
+
+console.log(listItems2)
+
+// listItems2.forEach(item=> console.log(item.innerText)) // for Each is not a function, becasue its an HTML collection
+// we can use .forEach() on a Node List, but not on an HTML collection
+const listItemsArray = Array.from(listItems2);
+listItemsArray.forEach(item=> console.log(item.innerText))
+// HAS TO BE AN ARRAY FOR .FOREACH TO WORK
+
+const listItems3 = document.getElementsByTagName('li');
+console.log(listItems3) // HTML collection 

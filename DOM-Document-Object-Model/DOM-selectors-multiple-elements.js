1eb832719bf7b41b328bd82cc4092d802c7f796a
Luka <luka.luka.luka93@gmail.com>
2024-07-22 14:20:06 -0700

    changed the color of list item, removed one and changed the innerHTML

--- DOM-Document-Object-Model/DOM-selectors-multiple-elements.js
+++ DOM-Document-Object-Model/DOM-selectors-multiple-elements.js
@@ -7,4 +7,26 @@ const listItems = document.querySelectorAll(".item");
 
 console.log(listItems) // NodeList(4) [li.item, li.item, li.item, li.item]
 console.log(listItems[1]) // This is getting me the second list item: <li class='item'>...</li>
-console.log(listItems[1].innerText)
+console.log(listItems[1].innerText) // Orange Juice
+
+// listItems.style.color = 'red'   - This will NOT work, becasue I am trying to access Style
+// on a Node List, style is for a specific element, so If I wanted to change all of them
+// I could use .forEach() loop
+
+listItems.forEach((item, index)=> {
+    item.style.color = 'red'; // colors all item lists into red
+    
+    if (index === 1){
+        item.remove() // it removes one item
+    } 
+    if(index === 0) {
+        // item.innerText = 'Oranges' // It changed the text Into Oranges, however:
+        // it also removed the X sign for delete, becasue that item list also includes
+        // the Delete button with that X logo inside, so what we can do is:
+        item.innerHTML = `Oranges
+          <button class="remove-item btn-link text-red">
+            <i class="fa-solid fa-xmark"></i>
+          </button>`;
+
+    }
+}) 

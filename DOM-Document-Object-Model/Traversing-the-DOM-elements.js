2cf604b5bf805bdcb39bfc110f9f623cf0469199
Luka <luka.luka.luka93@gmail.com>
2024-07-22 16:55:31 -0700

    get child elements

--- DOM-Document-Object-Model/Traversing-the-DOM-elements.js
+++ DOM-Document-Object-Model/Traversing-the-DOM-elements.js
@@ -7,6 +7,24 @@ There's certain properties if you want to work with those types of nodes
 
 /* ELEMENT NODES
 Any HTML tag on the page is an Element 
-
-
 */
+let output;
+// Get child Elements:
+const parent = document.querySelector('.parent'); // <div class='parent'>...</div>
+output = parent.children;  // children is not a method, its a property
+// HTMLCollection(3) [div.child, div.child, div.child]
+// becasue its an array looking structure I can:
+output = parent.children[1] //  <div class='child'>Child 2</div>
+// if I: console.dir(output) it will show me all the methods I can use on it
+output = parent.children[1].innerText; // Child 2
+output = parent.children[1].className; // child
+output = parent.children[1].nodeName; // DIV
+
+parent.children[1].innerText = 'Child Two';
+parent.children[1].style.color = 'Green';
+
+parent.firstElementChild.innerText = 'Child One';
+parent.lastElementChild.innerText = 'Child Three';
+
+
+console.log(output)

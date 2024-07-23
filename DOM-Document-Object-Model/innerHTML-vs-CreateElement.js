0bac0d16b7877a98db8e79c0d93b6caf89ed3667
Luka <luka.luka.luka93@gmail.com>
2024-07-23 16:44:58 -0700

    clean & Performant way of doing it

--- DOM-Document-Object-Model/innerHTML-vs-CreateElement.js
+++ DOM-Document-Object-Model/innerHTML-vs-CreateElement.js
@@ -9,9 +9,23 @@ function createListItem(item) {
           </button>`;
     document.querySelector('.items').appendChild(li);
 }
+// Using this inenrHTML makes it re-pass and recreate all the DOM nodes inside the UL element, so that is less efficient
 
 
 // Clean & Performant
+function createNewItem (item) {
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(item))
+    const button = document.createElement('button');
+    button.className = 'remove-item btn-link text-red';
+    const icon = document.createElement('i');
+    icon.className = 'fa-solid fa-xmark';
 
+    button.appendChild(icon);
+    li.appendChild(button); 
+    document.querySelector('.items').appendChild(li)
+    console.log(li.innerHTML) // // <button class="remove-item btn-link text-red"><i class="fa-solid fa-xmark"></i></button>
+}
 
-createListItem('Broccoli')
+createListItem('Broccoli');
+createNewItem('Tomato');

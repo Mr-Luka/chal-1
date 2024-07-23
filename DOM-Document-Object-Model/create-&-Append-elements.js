3f7e641091efb14254fc4f7658c45afe81d15c66
Luka <luka.luka.luka93@gmail.com>
2024-07-23 16:22:44 -0700

    create & append elements, created div and added class, id and title

---
+++ DOM-Document-Object-Model/create-&-Append-elements.js
@@ -0,0 +1,8 @@
+// shopping list html
+
+// Create an element and then append it
+ const div = document.createElement('div');
+ div.className = 'my-element';
+ div.id = 'my-element';
+ div.setAttribute('title', 'My Element') // <div class='my-element' id='my-element' title='My Element'></div>
+ console.log(div)

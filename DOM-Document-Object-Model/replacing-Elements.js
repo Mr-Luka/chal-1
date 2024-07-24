0fe4e2c0ec53a1f20fe74506490daff5f36c06a3
Luka <luka.luka.luka93@gmail.com>
2024-07-24 16:41:06 -0700

    shorter way

--- DOM-Document-Object-Model/replacing-Elements.js
+++ DOM-Document-Object-Model/replacing-Elements.js
@@ -18,3 +18,27 @@ function replaceSecondItem() {
     secondItem.outerHTML = '<li>Replaced Second</li>';
 }
 replaceSecondItem();
+
+
+
+// function replaceAllItems() {
+//     const lis = document.querySelectorAll('li');
+//     lis.forEach((li, index)=> {
+//         // li.outerHTML = '<li>Replace All</li>';
+//         if (index === 2) {
+//             li.innerHTML = 'Second Item'
+//         } else {
+//             li.innerHTML = 'Replace All'
+//         }
+//     })
+// }
+// replaceAllItems();
+
+//Shorter:
+function replaceAllItems() {
+    const lis = document.querySelectorAll('li');
+    lis.forEach((item, index)=> 
+        index === 2 ? item.innerHTML = 'Second Item' 
+        : item.innerHTML = 'Replace All' )
+}
+replaceAllItems();

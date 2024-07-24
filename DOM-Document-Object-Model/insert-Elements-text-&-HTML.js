bbccbed7cfa5c3f249069eddf66b85894f1ec3d9
Luka <luka.luka.luka93@gmail.com>
2024-07-24 15:16:51 -0700

    insertadjacent element, and text

--- DOM-Document-Object-Model/insert-Elements-text-&-HTML.js
+++ DOM-Document-Object-Model/insert-Elements-text-&-HTML.js
@@ -1,9 +1,24 @@
+// ELEMENT
 // insertAdjacentElement Example
+function insertElement() {
+    const filter = document.querySelector('.filter');
+    const h1 = document.createElement('h1');
+    h1.textContent = 'insertAdjacentElement';
 
+    filter.insertAdjacentElement('beforebegin', h1);
+    // it takes two parameters, first one where I want it, and second what element
+}
 
-// insertAdjacentText Example
+// TEXT 
+//insertAdjacentText Example
+function insertText() {
+    const item = document.querySelector('li:first-child');
 
+    item.insertAdjacentText('afterend', 'insertAdjacentText');
+    // i choose where to put it, and what text I want to put
+}
 
+// HTML
 // insertAdjacentHTML Example
 
 
@@ -11,6 +26,8 @@
 // insertBefore Example
 
 
+insertElement();
+insertText();
 /*
 <!-- beforebegin -->
 <p>

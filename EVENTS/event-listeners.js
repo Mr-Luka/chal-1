d61c76293da26b62efcbedc429fa4b31556d982b
Luka <luka.luka.luka93@gmail.com>
2024-07-25 13:57:38 -0700

    settimeout, and also mini challenge to remove all the items from the shopping list

--- EVENTS/event-listeners.js
+++ EVENTS/event-listeners.js
@@ -1,6 +1,11 @@
 const clearBtn = document.querySelector('#clear');
 // JavaScript Event Listener
 
+function onClear(){
+    alert('clear items')
+}
+
+
 // clearBtn.onclick = function () {
 //     alert('clear items')
 // }
@@ -8,5 +13,21 @@ const clearBtn = document.querySelector('#clear');
 
 
 // Recommended way, most modern: addEventListener()
-clearBtn.addEventListener("click", ()=> alert('clear items'))
-clearBtn.addEventListener("click", ()=> console.log('clear items'))
+// clearBtn.addEventListener("click", ()=> alert('clear items'))
+// clearBtn.addEventListener("click", onClear)
+// This way is I can have as many event listeners as I want.
+
+// setTimeout(()=> clearBtn.removeEventListener('click', onClear), 5000);
+// after 5 seconds, this event listener is being removed
+
+
+// setTimeout(()=> clearBtn.click(),5000); // it clicks the button after 5 seconds
+
+
+// Remove all the items from the shopping list
+function removeListItems() {
+    const listItems = document.querySelectorAll('.item');
+    listItems.forEach(item=> item.remove())
+}
+
+clearBtn.addEventListener("click", removeListItems);

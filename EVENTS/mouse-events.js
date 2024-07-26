0288dfbc134fdee3c894487f740e7348b8dbfe4e
Luka <luka.luka.luka93@gmail.com>
2024-07-25 17:26:23 -0700

    mousever, mouseout, dragstart, drag and drag end

--- EVENTS/mouse-events.js
+++ EVENTS/mouse-events.js
@@ -29,6 +29,27 @@ const onMouseUp = () => console.log('mouse up event')
 const onMouseWheel = () => console.log('mouse wheel event');
 
 
+// when I move with my mouse over it, it will activate the mouse over event
+const onMouseOver = () => console.log('mouse over event');
+
+
+// When I move with my mouse on top of the logo, and out of it, thats when its gonna be
+// activated
+const onMouseOut = () => console.log('mouse out event');
+
+// when I come to the logo, so when I click it and hold and try to move it,
+// it will trigger drag start event
+const onDragStart = () => console.log('drag start event')
+
+// when I click the logo its gonna fire drag event, and as long as I am holding the click, 
+// weather I move or not, its gonna keep firing the event, the faster I move, the faster
+// it will fire the event
+const onDrag = () => console.log('drag event')
+
+
+// When I click and hold the icon, the moment I reliease it will trigger onDragEnd event
+const onDragEnd = () => console.log('drag end event')
+
 
 // Event Listeners
 logo.addEventListener('click', onClick);
@@ -37,3 +58,10 @@ logo.addEventListener('contextmenu', onRightClick);
 logo.addEventListener('mousedown', onMouseDown);
 logo.addEventListener('mouseup', onMouseUp);
 logo.addEventListener('wheel', onMouseWheel);
+
+logo.addEventListener('mouseover', onMouseOver);
+logo.addEventListener('mouseout', onMouseOut);
+
+logo.addEventListener('dragstart', onDragStart);
+logo.addEventListener('drag', onDrag);
+logo.addEventListener('dragend', onDragEnd);

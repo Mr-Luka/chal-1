1e0891213a846afa47c2675b6d69cb3b9983b30d
Luka <luka.luka.luka93@gmail.com>
2024-07-26 11:17:15 -0700

    event objects

---
+++ EVENTS/event-object.js
@@ -0,0 +1,15 @@
+/*
+- `target` - The element that triggered the event
+- `currentTarget` - The element that the event listener is attached to 
+(These are the same in this case)
+- `type` - The type of event that was triggered
+- `timeStamp` - The time that the event was triggered
+- `clientX` - The X position of the mouse click relative to the window
+- `clientY` - The Y position of the mouse click relative to the window
+- `offsetX` - The X position of the mouse click relative to the element
+- `offsetY` - The Y position of the mouse click relative to the element
+- `pageX` - The X position of the mouse click relative to the page
+- `pageY` - The Y position of the mouse click relative to the page
+- `screenX` - The X position of the mouse click relative to the screen
+- `screenY` - The Y position of the mouse click relative to the screen
+*/

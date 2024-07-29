6b064eed76174f12163f084af03ea787c211c272
Luka <luka.luka.luka93@gmail.com>
2024-07-29 15:12:44 -0700

    adding one listener on parent element, testing whatever it is I am clicking on and then deliting it

--- EVENTS/event-delegation-&-Multiple-Events.js
+++ EVENTS/event-delegation-&-Multiple-Events.js
@@ -6,13 +6,29 @@ which is a powerful event handling pattern.
 */
 
 const listItems = document.querySelectorAll('li');
+const list = document.querySelector('ul'); // selecting the parent element
 
-listItems.forEach(item=> {
-    item.addEventListener('click', (e)=> {
-        e.target.remove(); // this way is not that good, because it will remove anything we click
-// inside the list item as well
+// listItems.forEach(item=> {
+//     item.addEventListener('click', (e)=> {
+//         e.target.remove(); // this way is not that good, because it will remove anything we click
+// // inside the list item as well
+// })
+
 /* Instead we can use event delegation to basically add just a single event listener onto the parent
 element, and then target whichever one we want to delete
 */
-    })
+
+list.addEventListener('click', (e)=> {
+    if(e.target.tagName === 'LI') {
+        e.target.remove();
+    }
+})
+/* Instead of adding event listeners on each item, I am going to a parent element UL, and 
+then testing whatever it is I am clicking on and then deliting that
+*/
+
+list.addEventListener('mouseover', (e)=> {
+    if(e.target.tagName === 'LI') {
+        e.target.style.color = 'red';
+    }
 })

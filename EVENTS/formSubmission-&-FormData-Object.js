6b3d3dd15c4becd6d26130ea3464abd1d045f53a
Luka <luka.luka.luka93@gmail.com>
2024-07-29 14:46:13 -0700

    form submission and form data object

--- EVENTS/formSubmission-&-FormData-Object.js
+++ EVENTS/formSubmission-&-FormData-Object.js
@@ -15,4 +15,24 @@ function onSubmit(e) {
     console.log(item, priority)
 }
 
-form.addEventListener('submit', onSubmit);
+function onSubmit2(e) {
+    e.preventDefault();
+
+    const formData = new FormData(form); // formData {}
+    // const item = formData.get('item'); // this Item, is actually the name: 'item'. in the <input> in HTML
+    // const priority = formData.get('priority');
+    // console.log(item, priority) // garlic 1
+
+    const entries = formData.entries(); // FormData Iterator {}
+    // console.log(entries)
+    for (let entrie of entries) {
+        // console.log(entrie) - it gives me an array of both the name and the priority:
+        // (2) ['item', 'garlic]
+        // (2) ['priority', '1']
+        console.log(entrie[1]);
+        // garlic
+        // 1
+    }
+}
+
+form.addEventListener('submit', onSubmit2);

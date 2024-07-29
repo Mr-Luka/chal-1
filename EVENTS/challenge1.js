49abc100f19b451277f25174a5932500f22fd5dd
Luka <luka.luka.luka93@gmail.com>
2024-07-29 13:07:13 -0700

    second way, where we removed the html, and then inserted it

--- EVENTS/challenge1.js
+++ EVENTS/challenge1.js
@@ -1,16 +1,40 @@
-const key = document.querySelectorAll('.key');
-console.log(key[0])
+// One way: did it on my own if the divs are still there
+// const key = document.querySelectorAll('.key');
+// console.log(key[0])
 
 
-function keybordPress (e) {
-    console.log(e.key, e.keyCode, e.code);
-    key[0].innerHTML = 
-    `${e.key} <small>e.key</small>`
-    key[1].innerHTML = 
-    `${e.keyCode}<small>e.keyCode</small>`
-    key[2].innerHTML = 
-    `${e.code}<small>e.code</small>`
+// function keybordPress (e) {
+//     console.log(e.key, e.keyCode, e.code);
+//     key[0].innerHTML = 
+//     `${e.key} <small>e.key</small>`
+//     key[1].innerHTML = 
+//     `${e.keyCode}<small>e.keyCode</small>`
+//     key[2].innerHTML = 
+//     `${e.code}<small>e.code</small>`
     
-}
+// }
+// window.addEventListener('keypress', keybordPress)
 
-window.addEventListener('keypress', keybordPress)
+// Second way: 
+// if the divs are removed, and we are making them here in #insert div
+
+window.addEventListener('keydown', (e)=> {
+    const insert = document.querySelector('#insert');
+
+    insert.innerHTML = `
+        <div class="key">
+        ${e.key === ' ' ? 'Space' : e.key}
+        <small>e.key</small>
+        </div>
+
+        <div class="key">
+        ${e.keyCode}
+        <small>e.keyCode</small>
+        </div>
+
+        <div class="key">
+        ${e.code}
+        <small>e.code</small>
+        </div>`;
+        
+})

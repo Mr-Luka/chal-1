dcd62c476b7e88469710b18743a880884acd041a
Luka <luka.luka.luka93@gmail.com>
2024-08-02 11:49:20 -0700

    movies.json

---
+++ Asynchronous-JavaScript/AJAX-XHR-Object.js
@@ -0,0 +1,8 @@
+/*              XMLHttp Request Object
+A build in browser object that allows us to make HTTP requests. We can make requests to
+servers without having to refresh the page. This allows us to make our webpages much more
+dynamic.
+These days, you will be usin the fetch API over XHR, however, Its still important to know 
+how to use it.
+
+*/

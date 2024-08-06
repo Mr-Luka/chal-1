2077173bf0c9fb1eb68eed1a4f0cc382e4b770dc
Luka <luka.luka.luka93@gmail.com>
2024-08-06 16:18:33 -0700

    catching the error on the function as a second way to do it, instead of in the function

--- Asynchronous-JavaScript/Fetch-API-&-Async-Await/error-handling-with-Async-&-Await.js
+++ Asynchronous-JavaScript/Fetch-API-&-Async-Await/error-handling-with-Async-&-Await.js
@@ -6,10 +6,29 @@ const getUsers = async () => {
         if(!response.ok) {
             throw new Error('Request failed')
         }
+
         const data = await response.text();
         console.log(data)
     } catch (error) {
         console.log(error);
     }
 }
-getUsers();
+// This is a good way to do it because it can catch the error before it runs, and that way
+// reacts how you want it with error. This way is prefered.
+// getUsers();
+
+
+// This other way how to catch the error, this way we just call it on the function that we call
+// after the function.
+const getPosts = async () => {
+        // const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        const response = await fetch('https://httpstat.us/500');
+
+        if(!response.ok) {
+            throw new Error('Request failed')
+        }
+
+        const data = await response.text();
+        console.log(data)
+}
+getPosts().catch(error=> console.log(error));

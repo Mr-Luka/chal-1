cd063edf6b7644976c674bb3fb93d7144afcd252
Luka <luka.luka.luka93@gmail.com>
2024-09-23 13:05:37 -0700

    making the blueprint with constructor, so we can create as many objects as we want

--- Constructors-&-Prototypes/constructor-functions.js
+++ Constructors-&-Prototypes/constructor-functions.js
@@ -13,11 +13,20 @@ Now what we want to do is be able to say New Rectangle, or new SHape, new User, 
 the object that we want to create.
 */
 
-function Rectangle() { // Whenever you create a constructor function, you want to have that first letter uppercase.
+function Rectangle(name, width, height) { // Whenever you create a constructor function, you want to have that first letter uppercase.
+/* Usually you will want some properties that you set, so you can set them by passing them in.
+Which means we of course have to have some parameters (name, width, height.... etc).
+*/
+    this.name = name;
+    this.width = width;
+    this.height = height;
+    this.area = function (){
+        return this.height * width
+    }
+} // This is pretty much the blueprint, so now I can create as much as I want.
 
-} 
-
-const rect1 = new Rectangle(); // if I didnt use New, it will be undefined
+const rect1 = new Rectangle('Rectangle 1', 10, 10); // if I didnt use New, it will be undefined
+// console.log: Rectangle {name: 'Rectangle 1', width: 10, height: 10}
 /* When we use the NEW keyword, theres actually 4 things that happen:
 
 1) A new empty object is created.
@@ -25,4 +34,11 @@ const rect1 = new Rectangle(); // if I didnt use New, it will be undefined
 3) The 'this' keyword is set to the new empty object.
 4) The new object is returned from the constructor function.
 */
-console.log(rect1)
+
+console.log(rect1.name);
+console.log(rect1.area())
+
+const rect2 = new Rectangle ('Rectangle 2', 20, 10);
+const rect3 = new Rectangle ('Rectangle 3', 30, 30);
+console.log(rect2.name, rect3.name);
+console.log(rect2.area(), rect3.area())

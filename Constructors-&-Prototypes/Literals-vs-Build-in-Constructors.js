28be67f289c4639b58b016ef1e6b501a6240a689
Luka <luka.luka.luka93@gmail.com>
2024-09-23 13:39:47 -0700

    built in constructor functions

--- Constructors-&-Prototypes/Literals-vs-Build-in-Constructors.js
+++ Constructors-&-Prototypes/Literals-vs-Build-in-Constructors.js
@@ -15,4 +15,40 @@ console.log(strLit.constructor); // String() { [native code] }
 console.log(strObj.constructor); // String() { [native code] }
 
 console.log(strLit instanceof String) // false/ because it's a literal we get false
-console.log(strObj instanceof String)
+console.log(strObj instanceof String) // true / cus it's an instance of string object
+
+// Other Types:
+
+const numLit = 20;
+const numObj = new Number(20);
+
+console.log(numLit, typeof numLit); // 20 'number'
+console.log(numObj, typeof numObj); // Number {20} 'object'
+
+const boolLit = true;
+const boolObj = new Boolean(true);
+
+console.log(boolLit, typeof boolLit); // true 'boolean'
+console.log(boolObj, typeof boolObj); // Boolean {true} 'object'
+
+const arrLit = [1, 2, 3, 4, 5];
+const arrObj = new Array(1, 2, 3, 4, 5);
+
+console.log(arrLit, typeof arrLit); // (5) [1, 2, 3, 4, 5] 'object'
+console.log(arrObj, typeof arrObj); // (5) [1, 2, 3, 4, 5] 'object'
+
+// function and object constructors
+const funcLit = function (x) {
+    return x * x;
+ };
+ console.log(funcLit, typeof funcLit); //(x){ return x * x; } 'function'   - it's an object
+console.log(funcLit(5)) // 25
+
+ const funcObj = new Function ('x', 'return x * x');
+console.log( funcObj(3)) // 9
+
+const obj1 = {};
+const obj2 = new Object();
+// When I write obj1 like that, its the same as obj2, it creates a new object
+console.log(obj1, typeof obj1) // {} 'object'
+console.log(obj2, typeof obj2) // {} 'object'

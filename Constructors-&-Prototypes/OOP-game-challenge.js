72c7f02ee37624fddb64f559823479bdc5e1791a
Luka <luka.luka.luka93@gmail.com>
2024-09-24 11:47:19 -0700

    OOP game challenge

---
+++ Constructors-&-Prototypes/OOP-game-challenge.js
@@ -0,0 +1,10 @@
+/* INSTRUCTIONS
+- Create a constructor to create a Player object with a name
+- Player should have a name as well as a LVL set to 1 by default and points set to 0 by default
+- Create a method on the prototype called gainXp that takes in a number from 1-10
+and adds it to the players points. If the current points are >= 10 then add 1 to the lvl
+and decrement the points by 10
+- Create another prototype method called describe that displays the players stats (name, lvl, points).
+*/
+
+

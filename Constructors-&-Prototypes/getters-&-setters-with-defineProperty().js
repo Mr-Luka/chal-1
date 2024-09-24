63a1fe3ee96f470e46859810809b7c4feee4f3f0
Luka <luka.luka.luka93@gmail.com>
2024-09-24 15:09:39 -0700

    object literal set and getter

--- Constructors-&-Prototypes/getters-&-setters-with-defineProperty().js
+++ Constructors-&-Prototypes/getters-&-setters-with-defineProperty().js
@@ -1,3 +1,4 @@
+// Constructor function
 function Person(firstName, lastName) {
     this._firstName = firstName;
     this._lastName = lastName;
@@ -7,7 +8,7 @@ function Person(firstName, lastName) {
 // and it takes an Object with a getter and a setter function
     Object.defineProperty(this, 'firstName', {
         get: function(){
-            return this._firstName;
+            return this.capitalizeFirst(this._firstName)
         },
         set: function(value) {
             this._firstName = value
@@ -16,11 +17,19 @@ function Person(firstName, lastName) {
 
     Object.defineProperty(this, 'lastName', {
     get: function(){
-        return this._lastName;
+        return this.capitalizeFirst(this._lastName)
     },
     set: function(value) {
         this._lastName = value
     }
+    });
+
+    Object.defineProperty(this, 'fullName', {
+    get: function(){
+        return this.firstName + ' ' + this.lastName
+// Becasue I am accessing firstName and lastName from getter and setter, it already knows
+// that it is capitalized
+    },
     });
 }
 
@@ -28,6 +37,36 @@ Person.prototype.capitalizeFirst = function (value) { // capitalizing first lett
     return value.charAt(0).toUpperCase() + value.slice(1);
 }
 
+// Object Literal
+const PersonObj = {
+    _firstName: 'jane',
+    _lastName:'doe',
+
+    get firstName() {
+        return Person.prototype.capitalizeFirst(this._firstName);
+    },
+    set firstName(value) {
+        this._firstName = value;
+    },
+
+    get lastName() {
+        return Person.prototype.capitalizeFirst(this._lastName);
+    },
+    set lastName(value) {
+        this._lastName = value;
+    },
+
+    get fullName() {
+        return this.firstName + ' ' + this.lastName;
+    }
+}
+
 const person1 = new Person('john', 'doe');
 console.log(person1.firstName) // firstName, without _, is becasue we defined it in Object.defineProperty()
 console.log(person1.lastName)
+console.log(person1.fullName) // John Doe
+
+const person2 = Object.create(PersonObj);
+console.log(person2.firstName) // Jane
+console.log(person2.lastName) // Doe
+console.log(person2.fullName) // Jane Doe

c1517d11f46416e741e9f8d69404896168f20b09
Luka <luka.luka.luka93@gmail.com>
2024-09-25 11:00:25 -0700

    similar example just with plants

--- Constructors-&-Prototypes/private-properties-convention.js
+++ Constructors-&-Prototypes/private-properties-convention.js
@@ -52,3 +52,59 @@ wallet.deposit(300);
 wallet.withdraw(50);
 console.log(wallet.balance); // 250
 console.log(wallet.transactions); // []
+
+
+
+// Another similar example:
+
+class PlantStore {
+    constructor () {
+        this._plants = 0;
+        this._transactions = [];
+    }
+
+    addPlants (amount) {
+        this._processAddedPlants(amount);
+        this._plants += amount;
+    }
+
+    gavePlants (amount) {
+        if(amount > this._plants){
+            console.log('Not enough plants!')
+        }
+        this._processGivenPlants(amount);
+        this._plants -= amount;
+    }
+
+    // Private method
+    _processAddedPlants (amount) {
+        console.log(`Adding ${amount} plants`);
+        this._transactions.push({
+            type: 'addPlants',
+            amount,
+        })
+    }
+
+    _processGivenPlants (amount) {
+        console.log(`Giving ${amount} plants`);
+        this._transactions.push({
+            type: 'gavePlants',
+            amount,
+        })
+    }
+
+
+    get balance(){
+        return this._plants;
+    }
+
+    get transactions(){
+        return this._transactions;
+    }
+
+}
+
+const bank = new PlantStore();
+bank.addPlants(100);
+bank.gavePlants(20);
+console.log(bank.transactions)

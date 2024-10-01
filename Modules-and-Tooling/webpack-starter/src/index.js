60eba909422c0082564c9a1280e5299713798eb8
Luka <luka.luka.luka93@gmail.com>
2024-10-01 15:17:28 -0700

    webpack configuration and npm run build

---
+++ Modules-and-Tooling/webpack-starter/src/index.js
@@ -0,0 +1,7 @@
+import message from './message';
+
+console.log(message)
+
+// we did then npm run buld in terminal,
+// that way in console log it didnt show an error anymore, because
+// it made main.js with bundled code
---
+++ Modules-and-Tooling/webpack-starter/webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'), // path that we are using for output is this 'dist' folder
+        filename: 'bundle.js' // this is the name of the file that will be generated
+    }
+}

98d7dc2aa0be13e2ea7ed05410ab7d36be18d62f
Luka <luka.luka.luka93@gmail.com>
2024-10-02 12:29:08 -0700

    css & style loaders

--- Modules-and-Tooling/webpack-starter/webpack.config.js
+++ Modules-and-Tooling/webpack-starter/webpack.config.js
@@ -6,5 +6,13 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'), // path that we are using for output is this 'dist' folder
         filename: 'bundle.js' // this is the name of the file that will be generated
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
     }
 }

d2c5c39db02e3cbd5da7d28b16d6a1e7ffea2597
Luka <luka.luka.luka93@gmail.com>
2024-10-02 13:06:13 -0700

    babel setup

--- Modules-and-Tooling/webpack-starter/src/index.js
+++ Modules-and-Tooling/webpack-starter/src/index.js
@@ -6,3 +6,5 @@ console.log(message)
 // we did then npm run buld in terminal,
 // that way in console log it didnt show an error anymore, because
 // it made main.js with bundled code
+
+const hello = () => console.log('Hello')

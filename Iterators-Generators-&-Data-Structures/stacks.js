568ada9d2d80be75dfbbff5246d3654b9724a7d0
Luka <luka.luka.luka93@gmail.com>
2024-10-06 11:49:08 -0700

    stacks, using push, pop, peek, length, isEmpty and clear:

--- Iterators-Generators-&-Data-Structures/stacks.js
+++ Iterators-Generators-&-Data-Structures/stacks.js
@@ -28,6 +28,47 @@ class Stack {
         this._items[this._count] = item;
         this._count++;
     }
+
+    pop() {
+        // this._items.pop() - easer method, or .slice(), but this time we are going the OG, 
+        // all manual
+        if(this.isEmpty()) {
+            return 'Underflow'
+        }  // in this case because we do stacked pop, we want the three to pop off, cus that was the last in
+
+        const item = this._items[this._count - 1];
+        this._count--;
+
+        for(let i = this._count; i < this._items.length; i++){
+            this._items[i] = this._items[i+1];
+// Taking each of the items and basically adding 1 to the index is it pushes them, it shifhts them
+// to the left, so we are getting rid of the last item, and then we are shifting the rest to the left.
+// That's what we are doing with this loop.
+        }
+        this._items.length = this._count;
+        return item;
+
+    }
+
+    peek() {
+        if(this.isEmpty()) {
+            return 'No Items in stack';
+        }
+        return this._items[this._count - 1];
+    }
+
+    isEmpty(){
+        return this._count === 0;
+    }
+
+    length(){
+        return this._count;
+    }
+
+    clear() {
+        this._items = [];
+        this._count = 0;
+    }
 }
 
 const stack = new Stack();
@@ -35,4 +76,12 @@ const stack = new Stack();
 stack.push('Item 1')
 stack.push('Item 2')
 stack.push('Item 3')
+stack.push('Item 4')
+stack.push('Item 5')
+
+stack.pop() // item 3 is gone
+stack.clear();
+console.log('Top Item: ', stack.peek()) 
+console.log('Stack Lenght: ', stack.length())
+
 console.log(stack)

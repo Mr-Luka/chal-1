a8bcdf932973ce674997985a2363898f267366c9
Luka <luka.luka.luka93@gmail.com>
2024-10-06 10:45:52 -0700

    maps

--- Iterators-Generators-&-Data-Structures/map.js
+++ Iterators-Generators-&-Data-Structures/map.js
@@ -18,3 +18,30 @@ console.log(myMap.has(1)); // true
 console.log(myMap.has(3)) // false
 myMap.delete(2);
 console.log(myMap.has(2))
+
+const peopleMap = new Map()
+peopleMap.set('brad', {phone: '555-555-5555', email: 'brad@gmail.com'});
+peopleMap.set('jack', {phone: '555-555-5555', email: 'jack@gmail.com'});
+peopleMap.set('jill', {phone: '555-555-5555', email: 'jill@gmail.com'});
+
+peopleMap.forEach(person=> console.log(person.email));
+// brad@gmail.com
+// jack@gmail.com
+// jill@gmail.com
+console.log(peopleMap.keys()) // MapIterator {'brad', 'jack', 'jill'}
+console.log(peopleMap.values()); // MapIterator {{...}, {...}, {...}}
+console.log(peopleMap.entries()); // MapIterator {'brad' => {...}, 'jack' => {...}}
+// since these are iterators, we can use next method:
+const iterator = peopleMap.values();
+console.log(iterator.next()) // {value: {...}. done: false}
+console.log(iterator.next()) // {value: {...}. done: false}
+console.log(iterator.next()) // {value: {...}. done: false}
+console.log(iterator.next()) // {value: {...}. done: true}
+
+// We can convert maps to arrays.
+const peopleArray = Array.from(peopleMap);
+console.log(peopleArray) // [Array(2), Array(2), Array(2)]
+// 0: (2) ['brad', {...}]
+// 0: 'brad'
+// 1: {phone: '555-555-5555', email: 'brad@gmail.com"}
+// length: 2

209d97607a29b24fde7b8b91ca5ae09927b130c2
Luka <luka.luka.luka93@gmail.com>
2024-10-09 13:16:54 -0700

    node.js = fs module

---
+++ Node.js-core-modules/fs.js
@@ -0,0 +1,54 @@
+/*
+ FS moduel allows us to use node.js to work with the local file system.
+ so if we want to create a file, if we want to read from a file, create folders, move files around,
+ then we can use this module
+
+Write to a file / creating and writing to a file
+There is an asynchronous version, which is the default
+and there's synchronous version.
+on top of that we can chose to use callbacks which is the default for asynchronous versions,
+or we can use promises.
+if you are gonna use promises its gonna be require('fs/promises)
+*/
+
+// const fs = require('fs');
+// const fs = require('fs/promises')
+
+// Write to a file //
+
+// Asynchronous version with the call back, whitch is the default
+// .         file name,   content for the file, and a callback(err)
+// fs.writeFile('file1.txt', 'Hello World', (err)=> {
+//     if(err) throw err;
+//     console.log('File created')
+// })
+// When I run node fs, it will say in console: File created, and in a folder
+// I will see a file called file1.txt
+
+
+// Promise version
+// fs.writeFile('file2.txt', 'Hello World 2')
+// .then(()=> console.log('File created'))
+// .catch((err)=> console.log(err))
+
+
+// Sync version
+// For the Synchronous version, you would use just the regular Fs
+// fs.writeFileSync('file3.txt', 'Hello World 3'); // anything that is bellow this line, is not
+// // gonna happen until this file has been created
+// console.log('File Created')
+
+
+// Promise version using Async await - What I would use personally 
+
+const fs = require('fs/promises')
+async function createFile(filename, content) {
+    try {
+        await fs.writeFile(filename, content);
+        console.log("File Created")
+    } catch(error) {
+        console.log(error)
+    }
+}
+
+createFile('file4.txt', 'Hello World 4')

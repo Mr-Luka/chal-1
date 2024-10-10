649d2e32720cec0c7826feab59c39e53d844a781
Luka <luka.luka.luka93@gmail.com>
2024-10-10 12:01:57 -0700

    making the api json

--- Node.js-core-modules/http.js
+++ Node.js-core-modules/http.js
@@ -6,21 +6,35 @@ environment. So for example it doesn't handle routing or sessions or static file
 create a production ready web server you wouold use a framework like express,
 */
 const http = require('http');
+const fs = require('fs')
+
+const posts = [
+    { id: 1, title: 'Post 1', body: 'This is Post 1' },
+    { id: 2, title: 'Post 2', body: 'This is Post 2' },
+]
 
 const server = http.createServer((request, response)=> { // .createServer() method
 //     response.end('Hello World') // .end() it will send the response and if I want to send something with it
 // // I can put 'Hello world'
 const url = request.url;
 if(url === '/') {
-    response.writeHead(200, { 'content-type': 'text/html'});
-    response.end('<h1>Welcome</h1>')
-} else if (url === '/') {
-    response.writeHead(200, { 'content-type': 'text/html'});
-    response.end('<h1>About</h1>')
-} else {
-    response.writeHead(404, { 'content-type': 'text/html'});
-    response.end('<h1>Page not found</h1>')
-}
+    fs.readFile('index.html', (error, file)=>{
+        if(error) {
+            response.writeHead(500, { 'content-type': 'text/html'});
+            response.end('<h1>Sorry we have a problem</h1>')
+        } else {
+            response.writeHead(200, { 'content-type': 'text/html'});
+            response.end(file)
+        }
+    })
+    } else if (url === '/api.posts') {
+        response.writeHead(200, { 'content-type': 'application/json'});
+        response.end(JSON.stringify({success: true, data: posts}))
+        
+    } else {
+        response.writeHead(404, { 'content-type': 'text/html'});
+        response.end('<h1>Page not found</h1>')
+    }   
 });
 
 server.listen(5000, ()=> {
